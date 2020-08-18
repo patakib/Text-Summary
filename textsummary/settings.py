aaae26c2ca73a649a446a409ab0eba9fc43c7c40
patbalint <patakibalintka@gmail.com>
2020-08-18 08:52:15 +0200

    creating django basis

--- textsummary/settings.py
+++ textsummary/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'textsummary_app.apps.TextsummaryAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
